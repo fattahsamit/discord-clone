288a2901dd424652e6f6717b0853a1c24563c21b
fattahsamit <fattahsamit@gmail.com>
2023-08-27 20:04:56 +0600

    Theme setup (Dark & Light)

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,10 +1,12 @@
 "use client";
+import { ModeToggle } from "@/components/mode-toggle";
 import { UserButton } from "@clerk/clerk-react";
 
 export default function Home() {
   return (
     <div>
       <UserButton afterSignOutUrl="/" />
+      <ModeToggle />
     </div>
   );
 }
