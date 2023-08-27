750921551ac3d20cc45c6a933ca3e733515b8e6d
fattahsamit <fattahsamit@gmail.com>
2023-08-27 19:29:31 +0600

    Authentication with clerk

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,15 +1,10 @@
-import { Button } from "@/components/ui/button";
+"use client";
+import { UserButton } from "@clerk/clerk-react";
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center gap-5 min-h-screen">
-      <p className="text-3xl font-bold text-indigo-500">Discord Clone</p>
-      <Button
-        variant={"ghost"}
-        className="bg-indigo-500 text-white text-md font-semibold hover:bg-indigo-600 hover:text-white"
-      >
-        Join
-      </Button>
+    <div>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
